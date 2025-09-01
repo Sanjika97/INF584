2af4e5e571d9146ae10099097cc0334492047919
Sanjika Perera <sanjikamanesha@gmail.com>
2025-09-02 01:55:58 +1000

    grid implimentation

--- LineUp/Grid.cs
+++ LineUp/Grid.cs
@@ -2,11 +2,38 @@ using System;
 
 namespace LineUp
 {
-    class Grid
+    public class Grid
     {
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
+        public int WinCondition { get; private set; }
         public Grid(int rows, int columns)
         {
-            
+            Rows = Math.Max(rows, 6);
+            Columns = Math.Max(columns, 7);
+
+            if (Rows > Columns)
+            {
+                Rows = Columns;
+            }
+
+            WinCondition = (int)Math.Floor(Rows * Columns * 0.1);
+        }
+
+        public void Display() {
+            for (int row = 0; row < Rows; row++)
+            {
+
+                for (int col = 0; col < Columns; col++)
+                {
+                    Console.Write("|");
+                    Console.Write("  ");
+                    
+                }
+            }
+
+            Console.WriteLine($"\nWin condition: Connect {WinCondition} discs");
         }
     }
+
 }
