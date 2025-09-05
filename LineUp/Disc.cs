e38fea5e50c1bb2abf0f771733b38091b9aef5ae
Sanjika Perera <sanjikamanesha@gmail.com>
2025-09-05 12:36:20 +1000

    unsaved code

--- LineUp/Disc.cs
+++ LineUp/Disc.cs
@@ -6,22 +6,22 @@ namespace LineUp
     {
         public char Symbol { get; private set; }
         public int PlayerNumber { get; private set; }
-        public string Type { get; private set; }
+        public DiscType Type { get; private set; }
 
-        public Disc(int playerNumber, string type)
+        public Disc(int playerNumber, DiscType type)
         {
             PlayerNumber = playerNumber;
             Type = type;
 
-            if (type == "Ordinary")
+            if (type == DiscType.Ordinary)
             {
                 Symbol = playerNumber == 1 ? '@' : '#';
             }
-            else if (type == "Boring")
+            else if (type == DiscType.Boring)
             {
                 Symbol = playerNumber == 1 ? 'B' : 'b';
             }
-            else if (type == "Magnetic")
+            else if (type == DiscType.Magnetic)
             {
                 Symbol = playerNumber == 1 ? 'M' : 'm';
             }
